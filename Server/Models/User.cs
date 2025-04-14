45f1427a1b9ac4916eaa3b12d7cc2f57355dee0c
zuzundrafx <workcel@yandex.ru>
2025-04-14 06:49:00 +0300

    Delete Server directory

--- Server/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace Server.Models;  // Пространство имён = Server.Models
-
-public class User
-{
-    public int Id { get; set; }
-    public long TelegramId { get; set; }
-    public required string Username { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
